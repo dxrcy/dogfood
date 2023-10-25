242666ff55542aa218898c7bcf50cee3ba3f3eae
darcy <acednes@gmail.com>
2023-10-25 20:40:16 +1100

    port to ibex

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use ibex::prelude::*;
 use ibex::{routes, ssg};
+use parse::{Entry, Verdict};
 
-const URL_ROOT: &str = "/ibex-template/";
+mod parse;
+
+const URL_ROOT: &str = "/dogfood/";
 
 fn main() {
-    let secret = "Hello!";
+    let entries = parse::get_entries();
 
     let routes = routes! [
-        (/)    => at_index(secret),
+        (/)    => at_index(entries),
         (/404) => at_404(),
     ];
 
@@ -15,43 +18,203 @@ fn main() {
     println!("All done!");
 }
 
-fn at_index(secret: &str) -> Document {
+fn at_index(entries: Vec<Entry>) -> Document {
     view! {
-        @use_basic[]
+        @use_basic
 
-        center {
-            h1 { "My Ibex App" }
-            p {
-                "This app was made using"
-                ~ a [href="https://github.com/darccyy/ibex-template"] {
-                    "Ibex Template"
+        div [class="columns"] {
+            div [class="short-list"] {
+                ul { [*for (entry) in (entries.iter()) {
+                    li { a [href=format!("#{}", name_to_id(&entry.name))] {
+                        [&entry.name]
+                    }}
+                }]}
+            }
+
+            div [class="long-list"] {
+                [*for (entry) in (entries.iter()) {
+                    @list_item [&entry, &entries]
+                }]
+            }
+
+            div [class="side-panel"] {
+                hr/
+                h1 { "In General" }
+                p { "The main human foods not to feed a dog are:" }
+                ul {
+                    li { a [href="#chocolate"]
+                        { "Chocolate" }}
+                    li { a [href="#xylitol"]
+                        { "Xylitol" } ~ "(Artificial sweetener)" }
+                    li { a [href="#grapes"]
+                        { "Grapes" }}
+                    li { a [href="#onion-garlic-chives-leeks"]
+                        { "Onions, garlic, ect" }}
+                }
+                div [class="disclaimer"] {
+                    h1 { "Disclaimer:" }
+                    p {
+                        "Please read the sources listed before trusting the information given."
+                        ~ "This website does not offer health advice."
+                    }
+                    p {
+                        "Submit any issues"
+                        ~ @short_link [
+                            "https://github.com/darccyy/dogfood/issues/new",
+                            Some("here"),
+                        ]
+                    }
                 }
             }
-            p {
-                "Secret message:" ~ i { "'" [secret] "'" }
+
+            a [class="top-button", href="#"] {
+                button { "↑" }
             }
-            p {
-                a [href=url!("not/a/real/path")] {
-                    "404 Example"
-                }
-            }
-            br/
-            img [
-                alt="Ibex logo",
-                src=url!("static/icon.png"),
-                width=300,
-            ]/
         }
     }
     .into()
 }
 
+fn list_item(entry: &Entry, entries: &[Entry]) -> View {
+    let id = name_to_id(&entry.name);
+    view! {
+        article [id=id, class="item"] {
+            hr/
+
+            h1 [class="name"] {
+                a [href=format!("#{}", id)] {
+                    [&entry.name]
+                    ~ "-" ~
+                    i { [match &entry.verdict {
+                        Verdict::Good  => "Yes!",
+                        Verdict::Bad   => "NO!",
+                        Verdict::Maybe => "Maybe...",
+                    }] }
+                }
+            }
+
+            [if let Some(subtitle) = &entry.subtitle { view! {
+                h4 [class="subtitle"] { [subtitle] }
+            }} else { view!{} }]
+
+            p [class="description"] { [&entry.description] }
+
+            div [class="sources"] {
+                [*if (!entry.sources.is_empty()) {
+                    p { i { "Sources:" } }
+                    ul { [*for (source) in (entry.sources.iter()) {
+                        li { @short_link [&source, None] }
+                    }] }
+                } else {
+                    p { b { "No sources!" } }
+                }]
+            }
+
+            [if let Some(review) = &entry.review { view!{
+                p [class="review"] {
+                    i { "Critic's Review:" }
+                    ~ span {
+                        [review]
+                    }
+                }
+            } } else { view!{} }]
+
+            div [class="related"] {
+                details {
+                    summary { "Related" }
+                    ul { [*for (other) in (entries.into_iter()) {
+                        [*if (
+                            other.name != entry.name
+                            && do_lists_intersect(&other.tags, &entry.tags)
+                        ) {
+                            li { a [href=format!("#{}", name_to_id(&other.name))] {
+                                [&other.name]
+                            } }
+                        }]
+                    }] }
+                }
+            }
+        }
+    }
+}
+
+fn do_lists_intersect<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    for a in a {
+        if b.contains(a) {
+            return true;
+        }
+    }
+    false
+}
+
+fn short_link(link: &str, text: Option<&str>) -> View {
+    // long version is for print
+    view! {
+        span [class="short-link"] {
+            [if let Some(text) = text { view! {
+                span [class="short"] {
+                    a [href=link] { [text] }
+                }
+                span [class="long", style="display:none"] {
+                    span [class="text"] { [text] }
+                    ":" ~
+                    a [href=link] { [link] }
+                }
+            }} else { view! {
+                span [class="short"] {
+                    a [href=link] { [shorten_url(link).unwrap_or(link)] }
+                }
+                span [class="long", style="display:none"] {
+                    a [href=link] { [link] }
+                }
+            }}]
+        }
+    }
+}
+
+fn shorten_url(link: &str) -> Option<&str> {
+    let mut split_protocol = link.split("://");
+    split_protocol.next();
+    let full_domain = split_protocol.next()?.split("/").next()?;
+    if !full_domain.starts_with("www.") {
+        return Some(full_domain);
+    }
+    let Some(index) = full_domain.find('.') else {
+        return Some(full_domain);
+    };
+    let (_, domain) = full_domain.split_at(index + 1);
+    if domain.is_empty() {
+        return Some(full_domain);
+    }
+    Some(domain)
+}
+
+/// Convert name into HTML #id attribute format, with limited character set
+fn name_to_id(name: &str) -> String {
+    name.to_lowercase()
+        .chars()
+        .map(|ch| {
+            if matches!(ch, 'a'..='z'|'0'..='9'|'_'|'-'|'+') {
+                ch
+            } else {
+                '-'
+            }
+        })
+        .collect::<String>()
+        .replace("--", "-")
+}
+
 fn at_404() -> Document {
     view! {
-        @use_basic[]
-
-        center {
-            "404 - Not found"
+        @use_basic
+        div [class="not-found"] {
+            h2 {
+                "... a 404 Error? -"
+                ~ i { "NO!" }
+            }
+            p { a [href=url!()] {
+                    "Back to the correct page?"
+            }}
         }
     }
     .into()
@@ -61,9 +224,13 @@ fn use_basic() -> View {
     view! {
         HEAD {
             @use_meta [Meta::new()]
-            title { "My Ibex App" }
+            title { "Can my dog eat...?" }
             link [rel="shortcut icon", href=url!("static/icon.png")]/
             link [rel="stylesheet", href=url!("css/main.css")]/
+        }
+
+        h1 [class="heading"] {
+            "Can my dog eat..."
         }
     }
 }
---
+++ src/parse.rs
@@ -0,0 +1,155 @@
+pub enum Verdict {
+    Good,
+    Bad,
+    Maybe,
+}
+
+impl TryFrom<&str> for Verdict {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "ok" => Ok(Self::Good),
+            "bad" => Ok(Self::Bad),
+            "maybe" => Ok(Self::Maybe),
+            _ => Err(()),
+        }
+    }
+}
+
+pub struct Entry {
+    pub name: String,
+    pub subtitle: Option<String>,
+    pub tags: Vec<String>,
+    pub verdict: Verdict,
+    pub description: String,
+    pub sources: Vec<String>,
+    pub review: Option<String>,
+}
+
+#[derive(Default)]
+struct EntryBuild<'a> {
+    pub name: Option<&'a str>,
+    pub subtitle: Option<&'a str>,
+    pub tags: Vec<&'a str>,
+    pub verdict: Option<Verdict>,
+    pub description: Vec<&'a str>,
+    pub sources: Vec<&'a str>,
+    pub review: Option<&'a str>,
+}
+
+pub fn get_entries() -> Vec<Entry> {
+    let file = include_str!("info.md");
+
+    let mut entries = Vec::new();
+    let mut entry_build = EntryBuild::default();
+
+    macro_rules! push_entry {
+        () => {
+            let name = entry_build.name.expect("No name given!").to_string();
+
+            entries.push(Entry {
+                subtitle: entry_build.subtitle.map(|x| x.to_string()),
+                tags: entry_build.tags.iter().map(|x| x.to_string()).collect(),
+                verdict: entry_build.verdict.expect(&format!(
+                    "No verdict given, or is invalid! Entry titled '{}'",
+                    name
+                )),
+                name,
+                description: entry_build.description.join("\n"),
+                sources: entry_build.sources.iter().map(|x| x.to_string()).collect(),
+                review: entry_build.review.map(|x| x.to_string()),
+            });
+        };
+    }
+
+    let mut first_heading_occurred = false;
+    for line in file.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+
+        let (token, rest) = split_token(line);
+
+        match token {
+            "#" => {
+                if first_heading_occurred {
+                    push_entry!();
+                }
+                first_heading_occurred = true;
+                entry_build = EntryBuild::default();
+                entry_build.name = Some(rest);
+            }
+
+            "##" => {
+                if entry_build.subtitle.is_some() {
+                    panic!(
+                        "Subtitle already given! Entry titled '{}'",
+                        entry_build.name.unwrap_or("[none]")
+                    );
+                }
+                entry_build.subtitle = Some(rest);
+            }
+
+            "`" => {
+                entry_build.tags = rest.split(',').map(|tag| tag.trim()).collect();
+            }
+
+            "-" => {
+                if entry_build.verdict.is_some() {
+                    panic!(
+                        "Verdict already given!, Entry titled '{}'",
+                        entry_build.name.unwrap_or("[none]")
+                    );
+                }
+                entry_build.verdict = rest.try_into().ok();
+            }
+
+            _ if is_ordered_ident(token) => {
+                entry_build.sources.push(rest);
+            }
+
+            ">" => {
+                if entry_build.review.is_some() {
+                    panic!(
+                        "Review already given!, Entry titled '{}'",
+                        entry_build.name.unwrap_or("[none]")
+                    );
+                }
+                entry_build.review = Some(rest);
+            }
+
+            _ => entry_build.description.push(line),
+        }
+    }
+
+    push_entry!();
+    entries
+}
+
+fn is_ordered_ident(token: &str) -> bool {
+    let mut chars = token.chars();
+    if chars.next_back() != Some('.') {
+        return false;
+    }
+    chars.as_str().parse::<u32>().is_ok()
+}
+
+fn split_token(line: &str) -> (&str, &str) {
+    let line = line.trim();
+    if line.starts_with('`') && line.ends_with('`') {
+        let mut chars = line.chars();
+        chars.next();
+        chars.next_back();
+        return ("`", chars.as_str());
+    }
+
+    match line.find(' ') {
+        Some(pos) => {
+            let (a, b) = line.split_at(pos);
+            (a.trim(), b.trim())
+        }
+        None => ("", line),
+    }
+}
